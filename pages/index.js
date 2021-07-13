f7a5637e3d15a1326c613bc53caaef306973de18
vimigueloli <vipeso2016@gmail.com>
2021-07-12 21:06:44 -0300

    perfil

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ function ProfileSidebar(propriedades) {
 }
 
 export default function Home() {
-  const usuarioAleatorio = 'omariosouto';
+  const usuarioAleatorio = 'vimigueloli';
   const pessoasFavoritas = [
     'juunegreiros',
     'omariosouto',
